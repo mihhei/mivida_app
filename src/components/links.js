32dc1461dabd099a2a7b76cedf6ca2bb1af7f1c2
mihhei <71437802+mihhei@users.noreply.github.com>
2021-03-15 15:55:12 +0200

    links bug remove

--- src/components/links.js
+++ src/components/links.js
@@ -8,7 +8,7 @@ export const Links = () => {
       <LinkTo linkName="Meistä" toBlock="bl1" scrollTime="1500" />
       <LinkTo linkName="Palvelut" toBlock="bl2" scrollTime="2000" />
       <LinkTo linkName="Tuotteet" toBlock="bl3" scrollTime="2500" />
-      <LinkTo linkName="Lahjakortti" toBlock="b4" scrollTime="3000" />
+      <LinkTo linkName="Lahjakortti" toBlock="bl4" scrollTime="3000" />
       <LinkTo linkName="Yhteystiedot" toBlock="bl5" scrollTime="3500" />
     </div>
   );
